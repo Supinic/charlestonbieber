1ea6bd33ce67866f7fcfd77e1c238d81b6f3e0a0
Çağla Pickaxe <caglapickaxe@protonmail.ch>
2020-04-12 19:16:40 +0300

    Add new command lyrics

---
+++ src/commands/Lyrics.ts
@@ -0,0 +1,50 @@
+import { Command, Input, Output } from '../modules';
+import got from 'got';
+
+interface APIResponse {
+  title: string;
+  author: string;
+  lyrics: string;
+  thumbnail: {
+    genius?: string;
+  };
+  links: {
+    genius: string;
+  };
+  error: string;
+}
+
+export class Lyrics extends Command {
+  name = 'lyrics';
+  aliases = ['shazam'];
+  cooldown = 10000;
+  data = null;
+
+  async execute(msg: Input, ...args: string[]): Promise<Output> {
+    const lyrics = args.join(' ');
+
+    if (!lyrics) {
+      return {
+        reply: 'Lyrics must be provided',
+        cooldown: 2500,
+        success: false,
+      };
+    }
+    
+    const data: APIResponse = await got({
+      url: 'https://some-random-api.ml/lyrics',
+      searchParams: { title: lyrics },
+    }).json();
+
+    if (data.error) {
+      return {
+        reply: 'eShrug',
+        cooldown: 5000,
+        success: false,
+      };
+    }
+
+    return { reply: `I guess that song is ${data.title} by ${data.author}` };
+  }
+}
+
