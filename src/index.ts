5929912ae010336d283701bc5e23ac168ab79304
Çağla Pickaxe <caglapickaxe@protonmail.ch>
2020-04-13 16:32:53 +0300

    Now pings supinic.com API

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { createConnection } from 'typeorm';
 import { Platform, User, Channel, Command, StaticCommand } from './modules';
+import { supi } from 'modules/GotInstances';
 
 createConnection().then(async connection => {
   await User.reload(connection);
@@ -9,3 +10,5 @@ createConnection().then(async connection => {
   Command.reload();
   Platform.reload();
 });
+
+setInterval(async () => await supi.put('bot-program/bot/active'), 36e5);
