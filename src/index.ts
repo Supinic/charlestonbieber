573064055b843bde6ccda592a158e95f0a522c8f
Çağla Pickaxe <caglapickaxe@protonmail.ch>
2020-04-14 12:38:47 +0300

    Dank commit

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,13 @@ import { Platform, User, Channel, Command, StaticCommand } from './modules';
 import { supi } from './modules/GotInstances';
 
 createConnection().then(async connection => {
-  await User.reload(connection);
-  await Channel.reload(connection);
-  await StaticCommand.reload(connection);
+  Channel.init(connection);
+  User.init(connection);
+
+  await StaticCommand.init(connection);
 
   Command.reload();
   Platform.reload();
-});
 
-setInterval(async () => await supi.put('bot-program/bot/active'), 36e5);
+  setInterval(async () => await supi.put('bot-program/bot/active'), 36e5);
+});
