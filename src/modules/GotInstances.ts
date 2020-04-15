06942e00fc339d5f02e390922f48a791b0c5e488
Çağla Pickaxe <caglapickaxe@protonmail.ch>
2020-04-15 23:37:59 +0300

    Add agent

--- src/modules/GotInstances.ts
+++ src/modules/GotInstances.ts
@@ -1,10 +1,15 @@
 import got from 'got';
 
-import { SUPIBOT_ID, SUPI_API_KEY, BING_MAPS_KEY } from '../config.json';
+import {
+  SUPIBOT_ID, SUPI_API_KEY, BING_MAPS_KEY, TWITCH_USERNAME,
+} from '../config.json';
 
 export const supi = got.extend({
   prefixUrl: 'https://supinic.com/api',
-  headers: { Authorization: `Basic ${[SUPIBOT_ID, SUPI_API_KEY].join(':')}` },
+  headers: {
+    Authorization: `Basic ${[SUPIBOT_ID, SUPI_API_KEY].join(':')}`,
+    'User-Agent': TWITCH_USERNAME,
+  },
 });
 
 export const bingMaps = got.extend({
