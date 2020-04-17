ca54aaf9acbb5fca2b9dc0ca7af9457ba767a15a
Çağla Pickaxe <caglapickaxe@protonmail.ch>
2020-04-17 16:19:35 +0300

    Update GotInstances.ts

--- src/modules/GotInstances.ts
+++ src/modules/GotInstances.ts
@@ -1,18 +1,16 @@
 import got from 'got';
 
-import {
-  SUPIBOT_ID, SUPI_API_KEY, BING_MAPS_KEY, TWITCH_USERNAME,
-} from '../config.json';
+import config from '../config.json';
 
 export const supi = got.extend({
   prefixUrl: 'https://supinic.com/api',
   headers: {
-    Authorization: `Basic ${[SUPIBOT_ID, SUPI_API_KEY].join(':')}`,
-    'User-Agent': TWITCH_USERNAME,
+    Authorization: `Basic ${[config.SUPIBOT_ID, config.SUPI_API_KEY].join(':')}`,
+    'User-Agent': config.TWITCH_USERNAME,
   },
 });
 
 export const bingMaps = got.extend({
   prefixUrl: 'https://dev.virtualearth.net/REST/v1',
-  searchParams: { key: BING_MAPS_KEY },
+  searchParams: { key: config.BING_MAPS_KEY },
 });
