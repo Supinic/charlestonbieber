00d010b39706a90e64476b5ab0985c1315aff5d0
Çağla Pickaxe <caglapickaxe@protonmail.ch>
2020-04-23 15:50:44 +0300

    Add optional argument: units

--- src/commands/Query.ts
+++ src/commands/Query.ts
@@ -6,15 +6,34 @@ export class Query extends Command {
   name = 'query';
   aliases = ['wolfram'];
   description = 'Solve a complex (or simple) problem, using Wolfram|Alpha';
-  syntax = ['problem'];
+  syntax = ['problem', 'units=?'];
   cooldown = 15000;
   data = null;
   permission = Permissions.EVERYONE;
 
   async execute(_msg: Input, ...args: string[]): Promise<Output> {
-    const problem = args.join(' ');
+    let searchParams = { appid };
 
-    if (!problem) {
+    for (let i = args.length - 1; i >= 0; i--) {
+      const token = args[i];
+
+      if (token.startsWith('units=')) {
+        const [option, value] = token.split('=');
+        searchParams[option] = value;
+
+        args.splice(i, 1);
+      }
+    }
+
+    if (!['metric', 'imperial'].includes(searchParams['units'])) {
+      return {
+        reply: '"units" must be "metric" or "imperial"',
+        success: false,
+        cooldown: 5000,
+      };
+    }
+
+    if (!args.length) {
       return {
         reply: 'A problem must be provided',
         success: false,
@@ -22,13 +41,12 @@ export class Query extends Command {
       };
     }
 
+    searchParams['i'] = args.join(' ');
+
     const answer = await got({
       url: 'https://api.wolframalpha.com/v1/result',
       throwHttpErrors: false,
-      searchParams: {
-        i: problem,
-        appid,
-      },
+      searchParams,
     }).text();
 
     return { reply: answer };
