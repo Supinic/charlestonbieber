f7744484025887e869dae6fec1657530a82485a2
Çağla Pickaxe <caglapickaxe@protonmail.ch>
2020-05-21 15:29:00 +0300

    Rename StaticComand.ts to StaticCommand.ts

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,7 +1,7 @@
 export { Channel } from './Channel';
 export { User } from './User';
 export { MCUVerse } from './MCUVerse';
-export { StaticCommand } from './StaticComand';
+export { StaticCommand } from './StaticCommand';
 export { Suggestion } from './Suggestion';
 export { Banphrase } from './Banphrase';
 export { AFK } from './AFK';
